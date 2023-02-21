6e727b1d0a3ccc571e27a7b4e535f4240602e77f
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-02-20 20:01:08 -0600

    Propmt Question

---
+++ script.js
@@ -0,0 +1,26 @@
+let p1 = prompt("What's your name?");
+alert("Hey "+ UserName);
+
+let p2 = prompt("How old are you?");
+alert("You're old and getting older")
+
+let p3 = prompt("Which one is better soccer or football?");
+alert("GGGGGOOOOOOAAAAALLLLLLL");
+
+let p4 = prompt("What's your favorite anime?");
+alert("If it's not One Piece,you're a loser.");
+
+
+// function Question () {
+//   let p1 = prompt("What's your name?"); 
+//   alert("Hey "+ p1);
+
+//   let p2 = prompt("How old are you?");
+//   alert("You're old and getting older");
+
+//   let p3 = prompt("Which one is better soccer or football?");
+//   alert("GGGGGOOOOOOAAAAALLLLLLL");
+
+//   let p4 = prompt("What's your favorite anime?");
+//   alert("If it's not One Piece,you're a loser.");
+//}
