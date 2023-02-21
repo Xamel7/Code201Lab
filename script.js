ccc9d16032a4804c66065264395d7077dec65fb9
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-02-20 20:08:54 -0600

    Prompt Script

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ let p4 = prompt("What's your favorite anime?");
 alert("If it's not One Piece,you're a loser.");
 
 
-// function Question () {
-//   let p1 = prompt("What's your name?"); 
-//   alert("Hey "+ p1);
+function Question () {
+   let p1 = prompt("What's your name?"); 
+   alert("Hey "+ p1);
 
-//   let p2 = prompt("How old are you?");
-//   alert("You're old and getting older");
+   let p2 = prompt("How old are you?");
+   alert("You're old and getting older");
 
-//   let p3 = prompt("Which one is better soccer or football?");
-//   alert("GGGGGOOOOOOAAAAALLLLLLL");
+   let p3 = prompt("Which one is better soccer or football?");
+   alert("GGGGGOOOOOOAAAAALLLLLLL");
 
-//   let p4 = prompt("What's your favorite anime?");
-//   alert("If it's not One Piece,you're a loser.");
-//}
+   let p4 = prompt("What's your favorite anime?");
+   alert("If it's not One Piece,you're a loser.");
+}
